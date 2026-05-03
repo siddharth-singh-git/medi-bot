68fa7b209832f42a50282df48301c0664974b559
Siddharth Singh <siddharth.singh0195@gmail.com>
2026-05-03 16:33:13 +0530

    Uncomment import for HuggingFaceEmbeddings

--- src/helper.py
+++ src/helper.py
@@ -1,7 +1,7 @@
 from langchain_community.document_loaders import PyPDFLoader,DirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
-#from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.embeddings import HuggingFaceEmbeddings
 #Extract Data From the PDF File
 def document_loader(data):
     loader= DirectoryLoader(data,
